6af36a5aae2ddc748ced1a997278272724497e91
Marijus Siliunas <marijus.siliunas@gmail.com>
2017-05-07 17:22:16 +0300

    Add CORS authorization

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ const beerRoutes = require('./beer_routes');
 module.exports = function(app) {
 	app.use(function(req, res, next) {
 	  res.header("Access-Control-Allow-Origin", "*");
-	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
 	  next();
 	});
   beerRoutes(app);
